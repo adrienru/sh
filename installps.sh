ef8e3a0897e1fbdf1a91e2ac544f524de7945df6
adrienru <u25253967@utp.edu.pe>
2025-06-21 17:55:28 -0600

    Update installps.sh

--- installps.sh
+++ installps.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# 🔧 Instalación opcional de rclone (se puede quitar si no se necesita)
-echo "🔧 Instalando rclone..."
-sudo -v
-curl https://rclone.org/install.sh | sudo bash
-
 # 📥 Descargar y mover script principal "5"
 echo "📥 Descargando script principal..."
 curl -fsSL https://raw.githubusercontent.com/adrienru/sh/refs/heads/main/5 -o /tmp/5
